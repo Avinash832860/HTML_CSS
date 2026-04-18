9fd026693f5e6b263e1accecf177b79cf0eaf7b1
Avinash Reddy <havinashreddy123@gmail.com>
2026-04-18 18:18:36 +0530

    my sample javascript

---
+++ sample.js
@@ -0,0 +1,7 @@
+const students = ["Avinash", "Anil", "Yaswanth","Swetha","N"];
+const roll_no=[1,20,30,40,50]
+console.log(students[1]);
+text=""
+for (let i = 0; i < students.length; i++) {
+  text += students[i];
+}
